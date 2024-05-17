6a6b222ddde6cdf43362c6a38aa64f63c0c9e020
TakumiPanda <danieyll.wilson@gmail.com>
2024-05-17 13:56:59 -0700

    basic player movement and create pause menu tester scene

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+
+    public float m_speed = 10.0f;
+    public float m_rotationspeed = 90.0f;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() { }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float m_vertialInput = Input.GetAxis("Vertical");
+        float m_horizontalInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.up * Time.deltaTime * m_speed * m_vertialInput);
+        transform.Rotate(Vector3.back, Time.deltaTime * m_rotationspeed * m_horizontalInput);
+
     }
 }
