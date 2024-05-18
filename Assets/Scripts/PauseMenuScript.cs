95640de7bf3ae95928abc7ffadfe29d7a21eacea
TakumiPanda <danieyll.wilson@gmail.com>
2024-05-18 11:02:19 -0700

    working pause menu in tester scene

---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using UnityEngine.SceneManager;
+
+public class PauseMenuScript : MonoBehaviour
+{
+
+    public GameObject m_pausemenuUI;
+
+    private bool m_isPaused = false;
+
+    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(m_isPaused)
+            {
+                Resume();
+            }
+            else 
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        m_pausemenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        m_isPaused = false;
+    }
+
+    void Pause()
+    {
+        m_pausemenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        m_isPaused = true;
+    }
+
+    public void ReturnMainMenu()
+    {
+        Time.timeScale = 1.0f;
+        Debug.Log("Going to Main Menu");
+        //TODO: SceneManager.LoadScene("MainMenu") or SceneManager.LoadScene(#)
+    }
+
+    public void Options()
+    {
+        Debug.Log("Opened Options page");
+    }
+}
