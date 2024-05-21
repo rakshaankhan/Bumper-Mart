f383a4a15a403d3ae393c2991e300453df5e80f9
TakumiPanda <danieyll.wilson@gmail.com>
2024-05-21 12:30:36 -0700

    added working int timerUI for gameplay

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    public float m_desiredTimer = 0;
+    private float m_deltaTime = 0;  
+
+    private GameObject m_TimerUI;  
+
+    // Start is called before the first frame update
+    void Start() 
+    { 
+        m_TimerUI = GameObject.FindWithTag("TimerUI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_deltaTime += Time.deltaTime;
+        int m_timeChanged = (int) (m_desiredTimer - m_deltaTime);
+        // Debug.Log(m_desiredTimer - m_deltaTime);
+        if(m_timeChanged <= 0)
+        {
+            m_timeChanged = 0;
+            //TODO: Load Failed Ending
+        }
+        m_TimerUI.GetComponent<TextMeshProUGUI>().text = "Time before parents get home: " + m_timeChanged;
+
+    }
+}
