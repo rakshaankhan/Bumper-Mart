765c6d6d99ce7ee2fd9f51a6acfcf3d7c5036cad
Npelletier91 <nicolas.pelletier@yahoo.com>
2024-05-24 18:08:05 -0400

    Shelves and their icons upgrade
    Shelves now change sprite when bumped
    Shelves now shake when bumped
    Icons disappear when collected
    I moved some sprites in different folders

--- Assets/Scripts/Shelf.cs
+++ Assets/Scripts/Shelf.cs
@@ -1,12 +1,52 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class Shelf : MonoBehaviour
 {
-    // assign the item type for each shelf in the inspector
-    public ItemType itemType;
+    public ItemType itemType;     // assign the item type for each shelf in the inspector
+    public GameObject icon;     // attach the icon to each shelf to allow disabling
+    public Sprite brokenSprite;
 
 
-    // This method tells us what type of item is assigned to each shelf
+    public float shakeDuration = 1.2f;
+    public float shakeMagnitude = 2.1f;
+
+    public void DisableIcon()
+    {
+        icon.SetActive(false);
+;    }
+
+    public void BrokenSprite()
+    {
+        SpriteRenderer iconSpriteRenderer = GetComponent<SpriteRenderer>();
+        iconSpriteRenderer.sprite = brokenSprite;
+
+        StartCoroutine(Shake());
+    }
+
+
+
+
+    private IEnumerator Shake()     // SHAKE EFFECT! 
+    {
+        Vector3 originalPosition = transform.localPosition;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < shakeDuration)
+        {
+            float offsetX = Random.Range(-1f, 1f) * shakeMagnitude;
+            float offsetY = Random.Range(-1f, 1f) * shakeMagnitude;
+
+            transform.localPosition = new Vector3(originalPosition.x + offsetX, originalPosition.y + offsetY, originalPosition.z);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.localPosition = originalPosition;
+    }
+
     // It is called on player controller
     public ItemType GetItemType()
     {
